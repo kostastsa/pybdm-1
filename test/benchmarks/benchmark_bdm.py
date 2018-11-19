9e044bef8f831a8316ff89f932ab3f7d597b3abc
sztal <stalaga@protonmail.com>
2018-11-19 23:00:36 +0100

    Add benchmark* prefix to pytest test discovery patterns

--- test/benchmarks/benchmark_bdm.py
+++ test/benchmarks/benchmark_bdm.py
@@ -5,5 +5,5 @@ def some_function():
     pass
 
 @pytest.mark.benchmark(min_rounds=30)
-def test_benchmark_some_function(benchmark):
+def benchmark_some_function(benchmark):
     benchmark(some_function)
