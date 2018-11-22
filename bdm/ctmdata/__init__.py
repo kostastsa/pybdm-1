b92c412802a206c0e66c92bf0837dfaab6f192d3
sztal <stalaga@protonmail.com>
2018-11-22 21:48:41 +0100

    Add basic documentation on CTM datasets

---
+++ bdm/ctmdata/__init__.py
@@ -0,0 +1,13 @@
+"""Resources submodule with reference dataset containing
+precomputed approximated algorithmic complexity values for
+simple objects based on the *Coding Theorem Method*.
+
+All datasets' names use the following naming scheme: ``ctm-bX-dY``.
+
+Datasets
+--------
+:``ctm-b2-d12.pickle``:
+    Binary strings of length from 1 to 12.
+:``ctm-b2-d4x4.pickle``:
+    Square binary matrices of width from 1 to 4.
+"""
