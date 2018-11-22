57ca42da642e9de8be5821b7faff60ff597a7b1e
sztal <stalaga@protonmail.com>
2018-11-22 13:55:07 +0100

    Use proper (from top to bottom powers) ordering in integer encoding of arrays

--- bdm/encoding.py
+++ bdm/encoding.py
@@ -7,6 +7,7 @@ and the set of non-negative integers.
 This technique is useful for compression of CTM datasets and makes it
 easier to specify simple input data for unit tests etc.
 """
+from collections import deque
 import numpy as np
 
 
@@ -52,7 +53,6 @@ def encode_sequence(seq, base=2):
         Encoding base.
         Should be equal to the number of unique symbols in the alphabet.
     """
-    # TODO: this should be rewritten as a numpy ufunc and piped into reduce
     if seq.size == 0:
         return 0
     if seq.ndim != 1:
@@ -65,10 +65,31 @@ def encode_sequence(seq, base=2):
     if not np.isin(seq, proper_valeus).all():
         raise ValueError(f"There are symbol codes greater than {base-1}")
     code = 0
-    for i, x in enumerate(seq):
+    for i, x in enumerate(reversed(seq)):
         if x > 0:
             code += x * base**i
     return code
+
+def decode_sequence(code, base=2, min_length=None):
+    """Decode sequence from a sequence code.
+
+    Parameters
+    ----------
+    code : int
+        Non-negative integer.
+    base : int
+        Encoding base.
+        Should be equal to the number of unique symbols in the alphabet.
+    """
+    bits = deque()
+    while code > 0:
+        code, rest = divmod(code, base)
+        bits.appendleft(rest)
+    n = len(bits)
+    if min_length and n < min_length:
+        for _ in range(min_length - n):
+            bits.appendleft(0)
+    return np.array(bits)
 
 def encode_array(x, base=2, **kwds):
     """Encode array of integer-symbols.
@@ -84,26 +105,6 @@ def encode_array(x, base=2, **kwds):
     """
     seq = np.ravel(x, **kwds)
     return encode_sequence(seq, base=base)
-
-def decode_sequence(code, base=2, min_length=None):
-    """Decode sequence from a sequence code.
-
-    Parameters
-    ----------
-    code : int
-        Non-negative integer.
-    base : int
-        Encoding base.
-        Should be equal to the number of unique symbols in the alphabet.
-    """
-    bits = []
-    while code > 0:
-        code, rest = divmod(code, base)
-        bits.append(rest)
-    n = len(bits)
-    if min_length and n < min_length:
-        bits += [ 0 for _ in range(min_length - n) ]
-    return np.array(bits)
 
 def decode_array(code, shape, base=2, **kwds):
     """Decode array of integer-symbols from a sequence code.
--- test/test_encoding.py
+++ test/test_encoding.py
@@ -25,7 +25,7 @@ def test_string_from_array(arr, expected):
 @pytest.mark.parametrize('seq,base,expected', [
     (np.array([]), 2, 0),
     (np.array([1, 0, 0, 1]), 2, 9),
-    (np.array([1, 0, 0, 1]), 3, 28)
+    (np.array([2, 0, 0, 1]), 3, 55)
 ])
 def test_encode_sequence(seq, base, expected):
     output = encode_sequence(seq, base=base)
@@ -35,7 +35,7 @@ def test_encode_sequence(seq, base, expected):
     (0, 2, None, np.array([])),
     (9, 2, None, np.array([1, 0, 0, 1])),
     (28, 3, None, np.array([1, 0, 0, 1])),
-    (20, 4, 5, np.array([0, 1, 1, 0, 0]))
+    (28, 4, 5, np.array([0, 0, 1, 3, 0]))
 ])
 def test_decode_sequence(code, base, min_length, expected):
     output = decode_sequence(code, base=base, min_length=min_length)
@@ -43,16 +43,16 @@ def test_decode_sequence(code, base, min_length, expected):
 
 @pytest.mark.parametrize('x,base,expected', [
     (np.array([]), 7, 0),
-    (np.array([0, 1, 1]), 2, 6),
-    (np.array([0, 1, 0, 1]), 4, 68)
+    (np.array([1, 1, 0]), 2, 6),
+    (np.array([0, 0, 1, 0, 1, 0]), 4, 68)
 ])
 def test_encode_array(x, base, expected):
     output = encode_array(x, base=base)
     assert output == expected
 
 @pytest.mark.parametrize('code,shape,base,expected', [
-    (17, (3, 3), 2, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 0]])),
-    (7, (2, 2), 3, np.array([[1, 2], [0, 0]]))
+    (17, (3, 3), 2, np.array([[0, 0, 0], [0, 1, 0], [0, 0, 1]])),
+    (7, (2, 2), 3, np.array([[0, 0], [2, 1]]))
 ])
 def test_decode_array(code, shape, base, expected):
     output = decode_array(code, shape, base=base)
