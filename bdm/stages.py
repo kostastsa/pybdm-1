952c16b564e3304f2a6d7ec12c227491bbc03af4
sztal <stalaga@protonmail.com>
2018-11-22 21:40:52 +0100

    Add proper docs for stages and utils modules

--- bdm/stages.py
+++ bdm/stages.py
@@ -1,26 +1,64 @@
-"""Block decomposition stage functions."""
+"""Block decomposition stage functions.
+
+Stage functions contain the main logic of the *Block Decomposition Method*
+and its different flavours depending on boundary conditions etc.
+The implementation follows the *split-apply-combine* approach.
+In the first stage an input dataset is partitioned into parts of shape
+appropriate for the selected CTM dataset (split stage).
+Next, approximate complexity for parts based on the *Coding Theorem Method*
+is looked up in the reference (apply stage) dataset.
+Finally, values for individual parts are aggregated into a final BDM value
+(combine stage).
+
+The partition (split) stage is implemented by the family of ``partition_*`` functions.
+The lookup (apply) stage is implemented by the family of ``lookup_*`` functions.
+The aggregate (combine) stage is implented by the family of ``aggregate_*`` functions.
+
+The general principle is that specific functions should in most cases
+be wrappers around the core family functions, which accordingly are:
+
+* :py:func:`bdm.stages.partition`
+* :py:func:`bdm.stages.lookup`
+* :py:func:`bdm.stages.aggregate`
+"""
 from collections import Counter
 import numpy as np
 from .encoding import string_from_array
 
 
 def partition(x, shape, shift=0):
-    """General dataset partition function.
+    """Core partition stage function.
+
+    It is implemented as a generator that yields subsequent parts.
 
     Parameters
     ----------
-    x : (N, k) array_like
-        Dataset.
+    x : array_like
+        Dataset of arbitrary dimensionality represented by a *Numpy* array.
     shape : tuple
         Shape of parts.
     shift : int
         Shift of the sliding window.
+        In general, if positive, should not be greater than ``1``.
         Shift by partition shape if not positive.
 
     Yields
     ------
     array_like
         Dataset parts.
+
+    Raises
+    ------
+    AttributeError
+        If parts' `shape` is equal in each dimension.
+        If parts' `shape` and dataset's shape are not conformable.
+
+    Examples
+    --------
+    >>> [ x for x in partition(np.ones((3, 3), dtype=int), shape=(2, 2)) ]
+    [array([[1, 1],
+           [1, 1]]), array([[1],
+           [1]]), array([[1, 1]]), array([[1]])]
     """
     if len(set(shape)) != 1:
         raise AttributeError(f"Partition shape is not symmetric {shape}")
@@ -48,10 +86,13 @@ def partition(x, shape, shift=0):
 def partition_ignore(x, shape):
     """Partition with ignore leftovers boundary condition.
 
+    In this variant parts that can not be further sliced and fitted into
+    the desired `shape` are simply omitted.
+
     Parameters
     ----------
-    x : (N, k) array_like
-        Dataset.
+    x : array_like
+        Dataset of arbitrary dimensionality represented by a *Numpy* array.
     shape : tuple
         Shape of parts.
 
@@ -59,18 +100,30 @@ def partition_ignore(x, shape):
     ------
     array_like
         Dataset parts.
+
+    Raises
+    ------
+    AttributeError
+        If parts' `shape` is equal in each dimension.
+        If parts' `shape` and dataset's shape are not conformable.
+
+    Examples
+    --------
+    >>> [ x for x in partition_ignore(np.ones((3, 3), dtype=int), shape=(2, 2)) ]
+    [array([[1, 1],
+           [1, 1]])]
     """
     for part in partition(x, shape, shift=0):
         if part.shape == shape:
             yield part
 
-def partition_shrink(x, shape, min_width=2):
+def partition_shrink(x, shape, min_width):
     """Partition with shrinking shape boundary condition.
 
     Parameters
     ----------
-    x (N, k) array_like
-        Dataset.
+    x array_like
+        Dataset of arbitrary dimensionality represented by a *Numpy* array.
     shape : tuple
         Shape of parts.
     min_width : int
@@ -80,6 +133,23 @@ def partition_shrink(x, shape, min_width=2):
     ------
     array_like
         Dataset parts.
+
+    Raises
+    ------
+    AttributeError
+        If parts' `shape` is equal in each dimension.
+        If parts' `shape` and dataset's shape are not conformable.
+
+    Examples
+    --------
+    >>> data = np.ones((5, 5), dtype=int)
+    >>> [ x for x in partition_shrink(data, shape=(3, 3), min_width=2)]
+    [array([[1, 1, 1],
+           [1, 1, 1],
+           [1, 1, 1]]), array([[1, 1],
+           [1, 1]]), array([[1, 1],
+           [1, 1]]), array([[1, 1],
+           [1, 1]])]
     """
     for part in partition(x, shape, shift=0):
         if part.shape == shape:
@@ -104,6 +174,20 @@ def lookup(parts, ctm):
     ------
     tuple
         2-tuple with string representation of a dataset part and its CTM value.
+
+    Raises
+    ------
+    KeyError
+        If key of an object can not be found in the reference CTM lookup table.
+
+    Examples
+    --------
+    >>> from bdm import BDM
+    >>> bdm = BDM(ndim=1)
+    >>> data = np.ones((16, ), dtype=int)
+    >>> parts = partition_shrink(data, (12, ), min_width=4)
+    >>> [ x for x in lookup(parts, bdm._ctm) ]
+    [('111111111111', 1.95207842085224e-08), ('1111', 0.00409810315977953)]
     """
     for part in parts:
         key = string_from_array(part)
@@ -129,6 +213,16 @@ def aggregate(ctms):
     -------
     float
         BDM value.
+
+    Examples
+    --------
+    >>> from bdm import BDM
+    >>> bdm = BDM(ndim=1)
+    >>> data = np.ones((40, ), dtype=int)
+    >>> parts = partition_shrink(data, (12, ), min_width=4)
+    >>> ctms = lookup(parts, bdm._ctm)
+    >>> aggregate(ctms)
+    1.5890606234017197
     """
     counter = Counter()
     for key, ctm in ctms:
--- bdm/utils.py
+++ bdm/utils.py
@@ -3,5 +3,11 @@ from . import _ctm_datasets
 
 
 def list_ctm_datasets():
-    """Get a list of available precomputed CTM datasets."""
+    """Get a list of available precomputed CTM datasets.
+
+    Examples
+    --------
+    >>> list_ctm_datasets()
+    ['ctm-b2-d12.pickle', 'ctm-b2-d4x4.pickle']
+    """
     return _ctm_datasets
